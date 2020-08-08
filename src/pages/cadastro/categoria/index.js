c4ce54b394eeb14a58f5d2f8bd599351a39c5cec
MarjadeSordi <marjadesordi@gmail.com>
2020-08-07 23:53:36 -0300

    marjaflix

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -29,6 +29,21 @@ function CadastroCategoria() {
       InfosdoEvento.target.value,
     );
   }
+
+  useEffect(() => {
+    if(window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias'; 
+      fetch(URL)
+       .then(async (respostaDoServer) =>{
+        if(respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          setCategorias(resposta);
+          return; 
+        }
+        throw new Error('Não foi possível pegar os dados');
+       })
+    }    
+  }, []);
 
   return (
     <PageDefault>
@@ -75,7 +90,11 @@ function CadastroCategoria() {
         <Button>
           Cadastrar
         </Button>
-      </form>
+      </form>7
+
+      {categorias.lenght ===0 && (<div>
+        Loading....
+      </div>)}
 
       <ul>
         {categorias.map((categoria) => (
@@ -91,6 +110,6 @@ function CadastroCategoria() {
       </Link>
     </PageDefault>
   );
-}
+} 
 
 export default CadastroCategoria;
